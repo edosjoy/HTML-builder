3812eb10d8427aea7d19f323900117e0f09d3323
Eduard <eduard.nabiulin@gmail.com>
2022-05-13 16:32:01 +0300

    01-read-file is done

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const stream = new fs.ReadStream('./01-read-file/text.txt', {encoding: 'utf-8'});
+
+stream.on('readable', () => {
+  const data = stream.read();
+  if (data !== null) {
+    console.log(data);
+  }
+});
